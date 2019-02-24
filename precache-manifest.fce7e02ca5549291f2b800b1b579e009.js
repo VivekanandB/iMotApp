ef1f13fab1f202ee2ed78459145c5574650bf1d4
VivekanandB <30481363+VivekanandB@users.noreply.github.com>
2019-02-24 07:26:07 +0530

    Updates

---
+++ precache-manifest.fce7e02ca5549291f2b800b1b579e009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec83f77bf4183f12646dd3eb780e26bc",
+    "url": "/iMotApp/static/media/logo.ec83f77b.jpg"
+  },
+  {
+    "revision": "f2ad071106d224ff7141",
+    "url": "/iMotApp/static/js/runtime~main.f2ad0711.js"
+  },
+  {
+    "revision": "e840454a6c112bccd142",
+    "url": "/iMotApp/static/js/main.e840454a.chunk.js"
+  },
+  {
+    "revision": "4ba74329f86868407e91",
+    "url": "/iMotApp/static/js/2.4ba74329.chunk.js"
+  },
+  {
+    "revision": "e840454a6c112bccd142",
+    "url": "/iMotApp/static/css/main.8c98d5ba.chunk.css"
+  },
+  {
+    "revision": "4ba74329f86868407e91",
+    "url": "/iMotApp/static/css/2.c66e895d.chunk.css"
+  },
+  {
+    "revision": "2f84b8ed57a3e548db01a7faa20a3b33",
+    "url": "/iMotApp/index.html"
+  }
+];
